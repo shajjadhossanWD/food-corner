356c3036b9e2904492e77b0cf026209a696d58ba
shajjadhossanWD <shajjadhossan111@gmail.com>
2021-08-30 22:39:51 +0600

    second

--- food.js
+++ food.js
@@ -16,7 +16,7 @@ const displayFood = meals =>{
          const newDiv = document.createElement('div');
          newDiv.classList.add('col');
          newDiv.innerHTML=`
-         <div class="card">
+         <div onclick="loadMealDetail(${food.idMeal})" class="card">
             <img src="${food.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">${food.strMeal}</h5>
@@ -27,4 +27,29 @@ const displayFood = meals =>{
          `
          foodDiv.appendChild(newDiv);
     })
+}
+
+const loadMealDetail = mealId  => {
+    // console.log(mealId);
+   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+   fetch(url)
+   .then(res => res.json())
+   .then(data => displayDetailsMeal(data.meals[0]))
+}
+
+const displayDetailsMeal = mealItem => {
+     console.log(mealItem)
+    const mealDetail = document.getElementById('meals-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+       <div class="card">
+           <img src="${mealItem.strMealThumb}" class="card-img-top" alt="...">
+           <div class="card-body">
+               <p class="card-text">${mealItem.strInstructions.slice(0, 250)}</p>
+           </div>
+           <a href= "${mealItem.strYoutube}" class="btn btn-info w-25 p-3" target="_blank" >saw vedio</a>
+       </div>
+    `;
+    mealDetail.appendChild(div);
 }
